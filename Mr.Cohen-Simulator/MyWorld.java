fa9fbe14b6855494472396eaedaf87273c608561
Waffles <waffles3438@gmail.com>
2024-04-05 18:08:26 -0400

    Update
    Renamed folder
    Added sounds to button

--- Mr.Cohen-Simulator/MyWorld.java
+++ Mr.Cohen-Simulator/MyWorld.java
@@ -18,9 +18,10 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(1280, 720, 1);
+        super(1260, 720, 1);
         addObject(start, getWidth()/2, getHeight()/2);
         addObject(settings, getWidth()/2, getHeight()/2 + 110);
         addObject(credits, getWidth()/2, getHeight()/2 + 220);
+        Button.init();
     }
 }
