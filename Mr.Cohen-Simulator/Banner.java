6d2ba3cfee4ad2c4e924a7c792e54ae0916b2671
Waffles <waffles3438@gmail.com>
2024-04-05 23:01:12 -0400

    Updated banner

---
+++ Mr.Cohen-Simulator/Banner.java
@@ -0,0 +1,12 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * 
+ * Design from: https://en.ac-illust.com/clip-art/1354907/pixelated-banner-set
+ * By: aimu
+ * 
+ */
+public class Banner extends Actor
+{
+    ;
+}
--- Mr.Cohen-Simulator/TitleScreen.java
+++ Mr.Cohen-Simulator/TitleScreen.java
@@ -1,10 +1,9 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Image from: https://pc98backgrounds.tumblr.com/image/172810349592
+ * 
  */
 public class TitleScreen extends World
 {
@@ -24,6 +23,7 @@ public class TitleScreen extends World
         addObject(start, getWidth()/2, getHeight()/2);
         addObject(settings, getWidth()/2, getHeight()/2 + 110);
         addObject(credits, getWidth()/2, getHeight()/2 + 220);
+        addObject(new Banner(), getWidth()/2, getHeight()/2 - 200);
         if(firstTime){
             Button.init();
             firstTime = false;
