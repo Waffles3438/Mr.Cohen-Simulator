53c416d858504391d942e0271719acf9344d81c4
BriftyNeeze <341039253@gapps.yrdsb.ca>
2024-04-06 14:57:03 -0400

    finished path finding
    still need them to move

--- Mr.Cohen-Simulator/Person.java
+++ Mr.Cohen-Simulator/Person.java
@@ -1,22 +1,31 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.util.PriorityQueue;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Queue;
+import java.util.LinkedList;
 
 /**
  * Write a description of class Person here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Felix Zhao
+ * @version April 6th 2024
  */
 public class Person extends SuperSmoothMover
 {
+    protected Queue<int[]> currentPath;
+    
+    public Person() {
+        currentPath = new LinkedList<int[]>();
+    }
     /**
      * Act - do whatever the Person wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
-        // Add your action code here.
+        
+        
     }
     
     public void pathFind(int targetX, int targetY) {
@@ -27,9 +36,9 @@ public class Person extends SuperSmoothMover
         int targetRow = targetY/10;
         int targetCol = targetX/10;
         // r, c for current Row and current col respectivly
-        int r = getY()/5*5;
-        int c = getX()/5*5;
-        PriorityQueue<int[]> openList = new PriorityQueue<int[]>();
+        int r = getY()/10;
+        int c = getX()/10;
+        PriorityQueue<int[]> openList = new PriorityQueue<int[]>((a, b) -> Integer.compare(a[0], b[0]));
         boolean[][] closedList = new boolean[totalRows][totalCols];
         Cell[][] cellData = new Cell[totalRows][totalCols];
         
@@ -46,6 +55,7 @@ public class Person extends SuperSmoothMover
         openList.add(new int[]{0, r, c});
         
         while (openList.size() != 0) {
+            
             int[] item = openList.poll();
             r = item[1];
             c = item[2];
@@ -53,7 +63,8 @@ public class Person extends SuperSmoothMover
                 continue;
             }
             closedList[r][c] = true;
-            if (r == targetRow && c == targetY) {
+            if (r == targetRow && c == targetCol) {
+                currentPath = new LinkedList<int[]>(tracePath(cellData, new int[]{r, c}));
                 break;
             }
             ArrayList<int[]> children = new ArrayList<int[]>();
@@ -88,13 +99,36 @@ public class Person extends SuperSmoothMover
                 int newH = Math.abs(targetRow-child[0]) + Math.abs(targetCol-child[1]);
                 int newF = newG + newH;
                 
+                if (cellData[child[0]][child[1]].getF() == -1 || cellData[child[0]][child[1]].getF() > newF) {
+                    openList.add(new int[]{newF, child[0], child[1]});
+                    cellData[child[0]][child[1]].setF(newF);
+                    cellData[child[0]][child[1]].setG(newG);
+                    cellData[child[0]][child[1]].setH(newH);
+                    cellData[child[0]][child[1]].setParent(child[0], child[1]);
+                }
+                    
                 
             }
         }
+        System.out.println("done");
     }
     
     public void pathFind(Actor actor) {
         pathFind(actor.getX(), actor.getY());
+    }
+    
+    public ArrayList<int[]> tracePath(Cell[][] cellData, int[] target) {
+        ArrayList<int[]> path = new ArrayList<int[]>();
+        int row = target[0];
+        int col = target[1];
+        while (!(cellData[row][col].getParent()[0] == row && cellData[row][col].getParent()[1] == col)) {
+            path.add(new int[]{row, col});
+            row = cellData[row][col].getParent()[0];
+            col = cellData[row][col].getParent()[1];
+        }
+        
+        Collections.reverse(path);
+        return path;
     }
 }
 
--- Mr.Cohen-Simulator/Simulator.java
+++ Mr.Cohen-Simulator/Simulator.java
@@ -6,14 +6,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class SimulationWorld extends World
+public class Simulator extends World
 {
     
     /**
      * Constructor for objects of class MyWorld.
      * 
      */
-    public SimulationWorld()
+    public Simulator()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1260, 720, 1); 
