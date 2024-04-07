3b04e9bf3840f6d17173ba1540322f341531925b
BriftyNeeze <341039253@gapps.yrdsb.ca>
2024-04-07 19:01:39 -0400

    added subclasses

---
+++ Mr.Cohen-Simulator/Alienware.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Alienware here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Alienware extends Computer
+{
+    public Alienware() {
+        
+    }
+    
+    /**
+     * Act - do whatever the Alienware wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
