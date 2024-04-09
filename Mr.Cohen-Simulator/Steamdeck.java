e1312bee9ff1a2c3eccba5b56cb28e7f2c048e88
AndyFeng070205 <150149712+AndyFeng070205@users.noreply.github.com>
2024-04-08 21:30:10 -0400

    hard code steamdeck

--- Mr.Cohen-Simulator/Steamdeck.java
+++ Mr.Cohen-Simulator/Steamdeck.java
@@ -12,6 +12,20 @@ public class Steamdeck extends Computer
      * Act - do whatever the Steamdeck wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public Steamdeck(double durabilityMultiplier){
+        super(durabilityMultiplier);
+        deviceImage = new GreenfootImage("images/SteamDeck.png");
+        //setImage(deviceImage);
+        deviceImage.scale(392, 156);
+        
+        screenImage = new GreenfootImage(deviceImage.getWidth() / 2 + 10, (int) deviceImage.getHeight() * 95 / 117);
+        screenImage.setColor(new Color(255, 100, 100));
+        screenImage.fillRect(0, 0, screenImage.getWidth(), screenImage.getHeight());
+        fullImage = new GreenfootImage(deviceImage);
+        fullImage.drawImage(screenImage, deviceImage.getWidth() / 2 - screenImage.getWidth() / 2 - 1, 14);
+        setImage(fullImage);
+    }
+    
     public void act()
     {
         // Add your action code here.
