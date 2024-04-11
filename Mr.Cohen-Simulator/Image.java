4c9fdf245cda83a944c66ff4d990455b6f8b3961
BriftyNeeze <341039253@gapps.yrdsb.ca>
2024-04-11 11:50:50 -0400

    update ratio

--- Mr.Cohen-Simulator/Image.java
+++ Mr.Cohen-Simulator/Image.java
@@ -4,36 +4,58 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * An actor that has no purpose other than displaying an image
  * 
  * Wooden banner: https://en.ac-illust.com/clip-art/1354907/pixelated-banner-set
+ * 
+ * @author Benny
+ * @author Felix
+ * @author Andy
  */
 public class Image extends Actor
 {
+    
+    private double ratio;
+    
     /**
      * @param imageName, name of the image
      */
-    private double ratio;
-    
-
     public Image(String imageName){
         setImage(imageName);
+        ratio = (double)getImage().getHeight() / getImage().getWidth();
     }
     
+    /**
+     * Creates image with a width and height
+     *
+     * @param width Width of image
+     * @param height Height of image
+     */
     public Image(int width, int height) {
         GreenfootImage image = new GreenfootImage(width, height);
         image.setColor(new Color(0, 0, 0));
         image.drawRect(0, 0, width-1, height-1);
         setImage(image);
+        ratio = (double)getImage().getHeight() / getImage().getWidth();
     }
     
     public Image(GreenfootImage image){
         setImage(image);
-        
+        ratio = (double)getImage().getHeight() / getImage().getWidth();
+        System.out.println(ratio);
     }
     
     public void act(){
         
     }
     
-    private void adjustSize(){
-        
+    public void adjustSize(int x){
+        getImage().scale(x, (int)(x*ratio));
     }
+    
+    /**
+     * Updates the ratio of the image. Call this when the image gets updated
+     *
+     */
+    public void updateRatio() {
+        ratio = (double)getImage().getHeight() / getImage().getWidth();
+    }
+    
 }
