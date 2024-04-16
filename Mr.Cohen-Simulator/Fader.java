1068a7fccf6e60b5bab39cbfb6f4c3c6c48b88f4
Waffles <waffles3438@gmail.com>
2024-04-15 20:54:32 -0400

    COOKING IN CS NOT IN NBE :fire:

--- Mr.Cohen-Simulator/Fader.java
+++ Mr.Cohen-Simulator/Fader.java
@@ -8,29 +8,60 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 
 public class Fader extends Actor{
-    private int maxTransparency = 250;
-    private int minTransparency = 0;
+    private int maxTransparency = 255;
+    private int minTransparency = 1;
     private int fadeSpeed = 1;
+    private boolean done = false;
+    private boolean finished = false;
     
     public Fader(String image, int max, int min, int speed){
         setImage(image);
         maxTransparency = max;
         minTransparency = min;  
         fadeSpeed = speed;
-        this.getImage().setTransparency(maxTransparency);
+        this.getImage().setTransparency(minTransparency);
     }
 
     public void fadeOut(){        
-        this.getImage().setTransparency(getImage().getTransparency() - fadeSpeed);
-        if (this.getImage().getTransparency() < (minTransparency + fadeSpeed) ){
+        if(getImage().getTransparency() - fadeSpeed >= minTransparency){
+            this.getImage().setTransparency(getImage().getTransparency() - fadeSpeed);
+        }
+        else{
+            sleepFor(30);
             getWorld().removeObject(this);
+            return;
         }
     }
     
     public void fadeIn(){        
-        this.getImage().setTransparency(getImage().getTransparency() + fadeSpeed);
-        if (this.getImage().getTransparency() > (maxTransparency + fadeSpeed) ){
-            return;
+        if(getImage().getTransparency() + fadeSpeed <= maxTransparency){
+            this.getImage().setTransparency(getImage().getTransparency() + fadeSpeed);
         }
+    }
+    
+    public void fadeInAndOut(){
+        if(!done){
+            if(getImage().getTransparency() + fadeSpeed <= maxTransparency){
+                this.getImage().setTransparency(getImage().getTransparency() + fadeSpeed);
+            }
+            
+            else{
+                done = true;
+                sleepFor(30);
+            }
+        } else{
+            if(getImage().getTransparency() - fadeSpeed >= minTransparency){
+                this.getImage().setTransparency(getImage().getTransparency() - fadeSpeed);
+            }
+            
+            else{
+                getWorld().removeObject(this);
+                return;
+            }
+        }
+    }
+    
+    public boolean getFinished(){
+        return finished;
     }
 }
