19584ba55b71d79ed7e048caa9a85b222b5d1c41
AndyFeng070205 <150149712+AndyFeng070205@users.noreply.github.com>
2024-04-23 11:22:12 -0400

    idk

---
+++ Mr.Cohen-Simulator/EndingOne.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class EndingOne here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class EndingOne extends FinishedWorld
+{
+
+    /**
+     * Constructor for objects of class EndingOne.
+     * 
+     */
+    public EndingOne()
+    {
+    }
+}
