feee451c364976701d9ad943a465895f1ae2e5cc
Waffles <waffles3438@gmail.com>
2024-04-25 23:08:27 -0400

    added typing sounds

--- Mr.Cohen-Simulator/PauseScreen.java
+++ Mr.Cohen-Simulator/PauseScreen.java
@@ -28,7 +28,6 @@ public class PauseScreen extends World
     private int previousVolume;
     /**
      * Constructor for objects of class PauseScreen.
-     * 
      */
     public PauseScreen(TitleScreen titleScreen, Simulator simulator, ArrayList<Actor> actors, Fader blackScreen)
     {    
@@ -75,8 +74,9 @@ public class PauseScreen extends World
             Greenfoot.setWorld(titleScreen);
         }
         if(resume.isPressed()) {
-            Simulator.setMusicVolume((int) volume/4);
+            Simulator.setMusicVolume((int) volume/7);
             Computer.setBreakingVolume((int) volume/10);
+            MrCohen.setTypingVolume(volume);
             Greenfoot.setWorld(simulator);
             resume.setPressedCondition(false);
         }
